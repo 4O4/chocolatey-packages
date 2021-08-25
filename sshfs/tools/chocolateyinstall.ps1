16dcd549de0748c4a13f4b7368c5d3ffe32bf1e4
Paweł K <4O4@rawbytes.net>
2021-08-25 08:47:02 +0200

    Update chocolateyinstall.ps1

--- sshfs/tools/chocolateyinstall.ps1
+++ sshfs/tools/chocolateyinstall.ps1
@@ -11,13 +11,9 @@ $packageArgs = @{
   file           = Get-Item $toolsDir\*-x86.msi
   file64         = Get-Item $toolsDir\*-x64.msi
   softwareName  = 'SSHFS-Win*'
-  checksum      = '9643DAF27EB7E384DADC9E64A4299C8D92E6C91E07913E9425F5B19AAD3B2DED'
-  checksumType  = 'sha256'
-  checksum64    = '1657E397F8DCE1C2D2E3220007F9C9F882631882B9BEC4608F7835E87DCD096C'
-  checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" ALLUSERS=1"
   validExitCodes= @(0, 3010, 1641)
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
